38c223f6894604030c44cbe14be04057b81269ca
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-11-06 16:18:23 +0100

    Remove `fsevents` dependency

---
+++ download-chokidar.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+CHOKIDAR_VERSION=2.1.8
+
+rm -rf chokidar
+mkdir chokidar
+
+npm pack "chokidar@$CHOKIDAR_VERSION"
+tar zxvfC chokidar-$CHOKIDAR_VERSION.tgz chokidar --strip 1 package
+
+rm chokidar-$CHOKIDAR_VERSION.tgz
+
+node -e "
+    const pkg = require('./package.json');
+    pkg.dependencies = require('./chokidar/package.json').dependencies;
+    fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2) + '\n');
+"
